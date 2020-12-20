7054d0a1c401a219fd66a65bfc30811516624be9
trongthoaivn <52898471+trongthoaivn@users.noreply.github.com>
2020-12-20 18:20:54 +0700

    Day 4

--- GUI/MainOpenCV.py
+++ GUI/MainOpenCV.py
@@ -1,14 +1,10 @@
 import sys
-import threading
-import time
 from datetime import datetime
-from timeit import Timer
 
-from IPython.external.qt_for_kernel import QtCore
+import cv2
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import QPropertyAnimation, QTimer
 from PyQt5.QtWidgets import QMessageBox, QLineEdit
-from js2py.base import false, true
 
 
 class frm_Main(QtWidgets.QMainWindow):
@@ -67,6 +63,10 @@ class frm_Main(QtWidgets.QMainWindow):
         # lb_User
         self.lb_User = self.findChild(QtWidgets.QLabel, 'lb_User')
 
+        # lb_Cam
+        self.lb_Cam = self.findChild(QtWidgets.QLabel, 'lb_cam')
+        # self.lb_Cam.setPixmap()
+
         self.show()
 
         timer = QTimer(self)
@@ -78,9 +78,63 @@ class frm_Main(QtWidgets.QMainWindow):
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         self.lb_Timer.setText("To Day: " + dt_string)
-        
+
     def getUsername(self, name):
         self.lb_User.setText("Welcome back! " + name)
+
+    def cam(self):
+        faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+        eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
+        smileCascade = cv2.CascadeClassifier('Cascades/haarcascade_smile.xml')
+
+        cap = cv2.VideoCapture(0)
+        cap.set(3, 640)  # set Width
+        cap.set(4, 480)  # set Height
+
+        while True:
+            ret, img = cap.read()
+            img = cv2.flip(img, 1)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.3,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                roi_gray = gray[y: y + h, x: x + w]
+                roi_color = img[y: y + h, x: x + w]
+
+                eyes = eyeCascade.detectMultiScale(
+                    roi_gray,
+                    scaleFactor=1.5,
+                    minNeighbors=5,
+                    minSize=(5, 5),
+                )
+
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+                smile = smileCascade.detectMultiScale(
+                    roi_gray,
+                    scaleFactor=1.5,
+                    minNeighbors=15,
+                    minSize=(25, 25),
+                )
+
+                for (xx, yy, ww, hh) in smile:
+                    cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 255, 0), 2)
+
+                cv2.imshow('video', img)
+
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:  # press 'ESC' to quit
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 class frm_Login(QtWidgets.QMainWindow):
