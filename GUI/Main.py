0db8205a4b8278cef7f3d738cda40efaf5ace792
trongthoaivn <52898471+trongthoaivn@users.noreply.github.com>
2021-01-15 20:50:19 +0700

    delete file test

--- GUI/Main.py
+++ GUI/Main.py
@@ -326,11 +326,6 @@ class frm_Main(QtWidgets.QMainWindow):
                     matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
                     name = "Unknown"
 
-                    # # If a match was found in known_face_encodings, just use the first one.
-                    # if True in matches:
-                    #     first_match_index = matches.index(True)
-                    #     name = known_face_names[first_match_index]
-
                     # Or instead, use the known face with the smallest distance to the new face
                     face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
                     best_match_index = np.argmin(face_distances)
